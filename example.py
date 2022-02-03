4aaf7776caa1b50427eb896aadfa552718544cfa
susu-dev <tndus9901@naver.com>
2022-02-03 17:04:45 +0900

    220203

--- example.py
+++ example.py
@@ -1,30 +1,17 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5 import uic # ui 파일을 사용하기 위한 모듈 import
 
-class MyWindow(QWidget):
+UI_class = uic.loadUiType("example.ui")[0]
+
+class MyWindow(QMainWindow, UI_class):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.setupUi(self)
 
-        # 윈도우 타이틀 변경
-        self.setWindowTitle("GUI TEST")
-
-        # 윈도우 크기 변경
-        self.setGeometry(300, 300, 400, 400)    # 순서대로 창 위치 x,y  창 크기 x,y
-
-    def initUI(self):
-        btn = QPushButton("Button", self)
-        btn.clicked.connect(self.surprise)
-
-    def surprise(self):
-        print("Button is Clicked")
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    window = MyWindow()
-    window.show()
-    app.exec_()     # 이벤트 루프 
-                    # '닫기' 이벤트 발생 전까지 계속 GUI 프로그램을 실행시키는 코드
-                    # app은 QApplication 클래스의 인스턴스
-app.exec_()
-print("루프 밖")
+app = QApplication(sys.argv)
+window = MyWindow()
+window.show()
+app.exec_()     # 이벤트 루프
+                # '닫기' 이벤트 발생 전까지 계속 GUI 프로그램을 실행시키는 코드
+                # app은 QApplication 클래스의 인스턴스
--- test.py
+++ test.py
@@ -8,13 +8,15 @@ form_class = uic.loadUiType("test.ui")[0]
 
 # 화면을 띄우는데 사용되는 Class 선언
 class WindowClass (QMainWindow, form_class) :
+
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         # test
 
-        self.btn_1.clicked.connect(self.button1Function)
-        self.btn_2.clicked.connect(self.button2Function)
+        # pushButtno (Start!)을 누르면 아래 functionStart 메소드와 연결됨
+        self.pushButton.clicked.connect(self.functionStart)
         """
         -------------------------------------------------------------
         이 부분에 시그널을 입력해야 합니다.
@@ -22,6 +24,25 @@ class WindowClass (QMainWindow, form_class) :
         -------------------------------------------------------------
         """
 
+    # Start! 버튼 눌렀을 때 실행되는 메소드
+    def functionStart(self):
+        self.progressBar.setRange(0, 19) # progressBar 초기 설정 (100을 0~19, 20단계로 나눔)
+
+        for i in range(1, 21):
+            print("출력 : ", str(i))
+            self.progressBar.setValue(i)    # progress bar 진행률 올리기
+            self.textBrowser.append("출력 : " + str(i))
+
+# 코드 실행시 GUI 창을 띄우는 부분
+# __name__ == "__main__" : 모듈로 활용되는 것이 아니라, 해당 .py파일에서 직접 실행되는 경우에만 코드 실행
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
+
+'''
 if __name__ == "__main__":
     # QApplication : 프로그램을 실행시켜주는 클래스
     app = QApplication(sys.argv)
@@ -34,3 +55,4 @@ if __name__ == "__main__":
 
     # 프로그램을 이벤트루프로 진입시키는 (프로그램을 작동시키는) 코드
     app.exec_()
+'''
